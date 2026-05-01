625e7cfce55f3b1be54f1a9087c406ba113961fb
y-yawnn <pattarin.sae@dome.tu.ac.th>
2026-05-01 22:39:47 +0700

    Edit mock data of animal and add mock data of staff

--- database/mock_data.sql
+++ database/mock_data.sql
@@ -50,97 +50,162 @@ SELECT 3, ZID FROM Zone;
 -- ==========================================
 -- ข้อมูลพนักงาน
 -- ==========================================
+-- Admin
 INSERT INTO Staff (Name, Surname, Nickname, Role, Username, Password, Phone, Sex, Salary, BirthDate, CAID)
 SELECT 
-    'Warinthorn', 'Milligan', 'Biome', 'Zookeeper','warinthorn', 'wxr1nth0npa22w0r4','0889886666', 'm', 59000.00, '2006-03-28',
+    'Natthaphon', 'Srisuk', 'Ton', 'Admin', 'natthaphon', 'n4tthap0n@dm1n', '0812345678', 'm', 75000.00, '1990-07-15',
     ca.CAID
-FROM Cage ca WHERE ZID = 1;
+FROM Cage ca WHERE ZID = 1 LIMIT 1;
+
+-- Veterinary Staff
+INSERT INTO Staff (Name, Surname, Nickname, Role, Username, Password, Phone, Sex, Salary, BirthDate, CAID)
+SELECT 
+    'Siriporn', 'Chaiyarat', 'Porn', 'Veterinary Staff', 'siriporn', 's1r1p0rn_v3t', '0823456789', 'f', 68000.00, '1995-11-02',
+    ca.CAID
+FROM Cage ca WHERE ZID = 1 LIMIT 1;
+
+-- Zookeeper
+INSERT INTO Staff (Name, Surname, Nickname, Role, Username, Password, Phone, Sex, Salary, BirthDate, CAID)
+SELECT 
+    'Kittiphat', 'Wongsri', 'Kit', 'Zookeeper', 'kittiphat', 'k1tt1ph4t_z00', '0834567890', 'm', 52000.00, '1998-04-20',
+    ca.CAID
+FROM Cage ca WHERE ZID = 1 LIMIT 1;
+
+-- Zookeeper (คนที่ 2 ตามโจทย์เดิม)
+INSERT INTO Staff (Name, Surname, Nickname, Role, Username, Password, Phone, Sex, Salary, BirthDate, CAID)
+SELECT 
+    'Warinthorn', 'Milligan', 'Biome', 'Zookeeper', 'warinthorn', 'wxr1nth0npa22w0r4', '0889886666', 'm', 59000.00, '2006-03-28',
+    ca.CAID
+FROM Cage ca WHERE ZID = 1 LIMIT 1;
 
 
 -- ==========================================
 -- ข้อมูลสัตว์ตัวอย่าง
 -- ==========================================
 
--- Mammals (Savanna)
+-- ------------------------------------------
+-- Mammalia
+-- ------------------------------------------
+
+-- 1. Snow Leopard
 INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
-SELECT 'African Lion','Panthera leo','Large social predator living in prides','m','2018-05-10',3,'Mammalia','Native to African savannas', c.CID, ca.CAID
+SELECT 'Snow Leopard','Panthera uncia','Elusive mountain predator with thick spotted fur','f','2019-03-22',1,'Mammalia','Found in mountain ranges of Central and South Asia', c.CID, ca.CAID
+FROM Category c, Cage ca, Zone z
+WHERE c.CName='Mammals' AND z.ZName='Mountain Zone' AND ca.ZID=z.ZID;
+
+-- 2. Bengal Tiger
+INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
+SELECT 'Bengal Tiger','Panthera tigris tigris','Largest wild cat species, powerful solitary hunter with striped coat','m','2019-11-25',2,'Mammalia','Native to the Indian subcontinent, endangered apex predator', c.CID, ca.CAID
+FROM Category c, Cage ca, Zone z
+WHERE c.CName='Mammals' AND z.ZName='Tropical Zone' AND ca.ZID=z.ZID;
+
+-- 3. Jaguar
+INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
+SELECT 'Jaguar','Panthera onca','Strongest big cat in the Americas, apex predator with rosette-patterned coat','m','2018-12-09',2,'Mammalia','Found across Central and South America, near threatened species', c.CID, ca.CAID
+FROM Category c, Cage ca, Zone z
+WHERE c.CName='Mammals' AND z.ZName='Tropical Zone' AND ca.ZID=z.ZID;
+
+-- 4. Cheetah
+INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
+SELECT 'Cheetah','Acinonyx jubatus','Fastest land animal, capable of reaching 112 km/h in short bursts','f','2021-05-20',3,'Mammalia','Native to Africa and parts of Iran, vulnerable species', c.CID, ca.CAID
 FROM Category c, Cage ca, Zone z
 WHERE c.CName='Mammals' AND z.ZName='Savanna Zone' AND ca.ZID=z.ZID;
 
+-- 5. Capybara
 INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
-SELECT 'African Bush Elephant','Loxodonta africana','Largest land mammal with trunk','f','2015-03-12',2,'Mammalia','Found in sub-Saharan Africa', c.CID, ca.CAID
+SELECT 'Capybara','Hydrochoerus hydrochaeris','World largest rodent, highly social and semi-aquatic','f','2021-09-03',5,'Mammalia','Native to South America, lives in groups near water bodies', c.CID, ca.CAID
 FROM Category c, Cage ca, Zone z
 WHERE c.CName='Mammals' AND z.ZName='Savanna Zone' AND ca.ZID=z.ZID;
 
+-- 6. Wolverine
 INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
-SELECT 'Pygmy Hippopotamus','Choeropsis liberiensis','Small semi-aquatic hippo','f','2024-07-10',1,'Mammalia','West African forests', c.CID, ca.CAID
+SELECT 'Wolverine','Gulo gulo','Fierce solitary land predator with immense strength for its size','m','2020-04-17',1,'Mammalia','Inhabits boreal forests and Arctic tundra of the Northern Hemisphere', c.CID, ca.CAID
 FROM Category c, Cage ca, Zone z
-WHERE c.CName='Mammals' AND z.ZName='Savanna Zone' AND ca.ZID=z.ZID;
+WHERE c.CName='Mammals' AND z.ZName='Arctic Zone' AND ca.ZID=z.ZID;
 
+-- ------------------------------------------
+-- Reptilia
+-- ------------------------------------------
+
+-- 7. Komodo Dragon
 INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
-SELECT 'Chimpanzee','Pan troglodytes','Highly intelligent primate','m','2020-05-12',2,'Mammalia','African forests', c.CID, ca.CAID
+SELECT 'Komodo Dragon','Varanus komodoensis','Largest living lizard with toxic saliva','m','2017-08-05',2,'Reptilia','Endemic to Indonesian islands, powerful ambush predator', c.CID, ca.CAID
 FROM Category c, Cage ca, Zone z
-WHERE c.CName='Mammals' AND z.ZName='Savanna Zone' AND ca.ZID=z.ZID;
+WHERE c.CName='Reptiles' AND z.ZName='Tropical Zone' AND ca.ZID=z.ZID;
 
+-- ------------------------------------------
+-- Amphibia
+-- ------------------------------------------
 
--- Birds (Arctic)
+-- 8. Axolotl
 INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
-SELECT 'Emperor Penguin','Aptenodytes forsteri','Largest penguin species','f','2020-03-15',10,'Aves','Antarctica', c.CID, ca.CAID
+SELECT 'Axolotl','Ambystoma mexicanum','Aquatic salamander with remarkable regeneration ability','f','2022-11-18',4,'Amphibia','Native to Lake Xochimilco in Mexico, critically endangered', c.CID, ca.CAID
 FROM Category c, Cage ca, Zone z
-WHERE c.CName='Birds' AND z.ZName='Arctic & Cold Zone' AND ca.ZID=z.ZID;
+WHERE c.CName='Amphibians' AND z.ZName='Aquatic Zone' AND ca.ZID=z.ZID;
 
+-- ------------------------------------------
+-- Aves
+-- ------------------------------------------
+
+-- 9. Harpy Eagle
 INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
-SELECT 'Golden Eagle','Aquila chrysaetos','Powerful bird of prey','m','2019-07-21',2,'Aves','Northern Hemisphere', c.CID, ca.CAID
+SELECT 'Harpy Eagle','Harpia harpyja','Apex aerial predator of the Amazon rainforest','f','2020-02-14',1,'Aves','One of the largest and most powerful eagles in the world', c.CID, ca.CAID
 FROM Category c, Cage ca, Zone z
-WHERE c.CName='Birds' AND z.ZName='Arctic & Cold Zone' AND ca.ZID=z.ZID;
+WHERE c.CName='Birds' AND z.ZName='Tropical Zone' AND ca.ZID=z.ZID;
 
+-- ------------------------------------------
+-- Fish
+-- ------------------------------------------
 
--- Reptiles (Tropical)
+-- 10. Manta Ray
 INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
-SELECT 'Burmese Python','Python bivittatus','Large constrictor snake','m','2019-07-21',2,'Reptilia','Southeast Asia', c.CID, ca.CAID
+SELECT 'Manta Ray','Mobula birostris','Largest ray species, filter feeder in open ocean','f','2018-07-30',1,'Fish','Circumglobal in tropical and subtropical waters', c.CID, ca.CAID
 FROM Category c, Cage ca, Zone z
-WHERE c.CName='Reptiles' AND z.ZName='Tropical Forest Zone' AND ca.ZID=z.ZID;
+WHERE c.CName='Fish' AND z.ZName='Aquatic Zone' AND ca.ZID=z.ZID;
 
+-- ------------------------------------------
+-- Cephalopoda
+-- ------------------------------------------
 
--- Amphibians (Tropical)
+-- 11. Blue-ringed Octopus
 INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
-SELECT 'Frog','Rana temporaria','Amphibian with jumping ability','f','2021-06-01',5,'Amphibia','Near water habitats', c.CID, ca.CAID
+SELECT 'Blue-ringed Octopus','Hapalochlaena lunulata','Small but highly venomous marine predator','m','2023-06-10',2,'Cephalopoda','Inhabits tide pools and coral reefs of the Pacific Ocean', c.CID, ca.CAID
 FROM Category c, Cage ca, Zone z
-WHERE c.CName='Amphibians' AND z.ZName='Tropical Forest Zone' AND ca.ZID=z.ZID;
+WHERE c.CName='Marine Invertebrates' AND z.ZName='Aquatic Zone' AND ca.ZID=z.ZID;
 
+-- ------------------------------------------
+-- Mythical
+-- ------------------------------------------
 
--- Aquatic (Ocean)
+-- 12. Phoenix
 INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
-SELECT 'Great White Shark','Carcharodon carcharias','Ocean predator','m','2017-09-09',1,'Fish','Apex predator', c.CID, ca.CAID
+SELECT 'Phoenix','Avis immortalis','Immortal firebird reborn from its own ashes','m','0001-01-01',1,'Mythical','Sacred fire bird of ancient Greek and Egyptian mythology', c.CID, ca.CAID
 FROM Category c, Cage ca, Zone z
-WHERE c.CName='Aquatic Animals' AND z.ZName='Ocean Realm' AND ca.ZID=z.ZID;
+WHERE c.CName='Mythical Creatures' AND z.ZName='Mythical Zone' AND ca.ZID=z.ZID;
 
+-- 13. Kirin
 INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
-SELECT 'Southern Stingray','Dasyatis americana','Flat-bodied ray','f','2018-11-11',3,'Fish','Shallow waters', c.CID, ca.CAID
+SELECT 'Kirin','Qilin benevolentus','Chimeric beast symbolizing prosperity and good omen','m','0001-01-01',1,'Mythical','Auspicious creature in Chinese, Japanese and Korean mythology', c.CID, ca.CAID
 FROM Category c, Cage ca, Zone z
-WHERE c.CName='Aquatic Animals' AND z.ZName='Ocean Realm' AND ca.ZID=z.ZID;
+WHERE c.CName='Mythical Creatures' AND z.ZName='Mythical Zone' AND ca.ZID=z.ZID;
 
-
--- Insects (Rainforest)
+-- 14. Kraken
 INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
-SELECT 'Monarch Butterfly','Danaus plexippus','Migratory butterfly','f','2022-04-01',20,'Insecta','Pollinator species', c.CID, ca.CAID
+SELECT 'Kraken','Cephalopodus giganteus','Colossal sea monster capable of sinking ships','m','0001-01-01',1,'Mythical','Legendary sea monster of Scandinavian folklore', c.CID, ca.CAID
 FROM Category c, Cage ca, Zone z
-WHERE c.CName='Insects' AND z.ZName='Rainforest Pavilion' AND ca.ZID=z.ZID;
+WHERE c.CName='Mythical Creatures' AND z.ZName='Mythical Zone' AND ca.ZID=z.ZID;
 
-
--- Fantasy Beasts (Bamboo)
+-- 15. Pegasus
 INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
-SELECT 'Celestial Unicorn','Equus unicornis','Magical horse','f','2022-01-01',1,'Mythical','Symbol of purity', c.CID, ca.CAID
+SELECT 'Pegasus','Equus alatus','Winged divine horse born from sea foam','m','0001-01-01',1,'Mythical','Divine winged stallion of Greek mythology, symbol of wisdom', c.CID, ca.CAID
 FROM Category c, Cage ca, Zone z
-WHERE c.CName='Fantasy Beasts' AND z.ZName='Bamboo Grove' AND ca.ZID=z.ZID;
+WHERE c.CName='Mythical Creatures' AND z.ZName='Mythical Zone' AND ca.ZID=z.ZID;
 
-
--- Legendary Creatures (Bamboo)
+-- 16. Unicorn
 INSERT INTO Animal (Name, SciName, BioCharacter, Sex, BirthDate, Quantity, Class, Description, CID, CAID)
-SELECT 'Eastern Dragon','Draco orientalis','Serpentine dragon','m','2020-10-10',1,'Mythical','Asian mythology', c.CID, ca.CAID
+SELECT 'Unicorn','Equus monocerus','Graceful horse with a single spiraling horn, symbol of purity and magic','f','0001-01-01',1,'Mythical','Legendary creature of European folklore, symbol of grace and purity', c.CID, ca.CAID
 FROM Category c, Cage ca, Zone z
-WHERE c.CName='Legendary Creatures' AND z.ZName='Bamboo Grove' AND ca.ZID=z.ZID;
-
+WHERE c.CName='Mythical Creatures' AND z.ZName='Mythical Zone' AND ca.ZID=z.ZID;
 
 -- ==========================================
 -- ความสัมพันธ์อาหาร
